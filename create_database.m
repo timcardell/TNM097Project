cb24cc1513d8f5ab0eb4eb293e81f6719ae09530
Tim Cardell <timcal04.04@gmail.com>
2020-02-07 14:45:12 +0100

    added database, convertion to lab

--- create_database.m
+++ create_database.m
@@ -1,19 +1,21 @@
 %% create database of images
 
     
-   for i= 1:10
+   for i= 1:242 %loop through all images, reaize and convert to lab
     
        DB{i} = imread(['DB (' int2str(i) ').jpg']);
        im_resized{i} = imresize(DB{i}, [25 25], 'bicubic');
-       
-       
-       img_lab{i} = rgb2lab(im_resized{i})
+       img_lab{i} = RGB2Lab(im_resized{i})
    end
-
+   
+   
+%% save database
+   save('database.mat', 'img_lab','im_resized')
+   
    
 %%
    
-   imshow(im_resized{1})
+
    
  
 
