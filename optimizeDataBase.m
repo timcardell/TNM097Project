f3b4d551ddea7a543ad5e20aee089bbe2230f4dd
Tim Cardell <timcal04.04@gmail.com>
2020-02-11 10:55:43 +0100

    added function to half the database

--- optimizeDataBase.m
+++ optimizeDataBase.m
@@ -1,6 +1,10 @@
-function [DB_optimized] = optimizeDataBase(DBLab, DBrgb)
+
+%input img_lab, im_resized
+function [DB_optimized] = optimizeDataBase(DBLab)
 
 
+for i = 1:1:125
+    DB_optimized{i} = DBLab{i}
 
 end
 
