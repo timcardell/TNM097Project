3b80bf2a2b9ed91a8fb80c8e0e212b8ce635b5f2
Tim Cardell <timcal04.04@gmail.com>
2020-02-12 10:00:05 +0100

    added function structure to database

---
+++ optimizedDatabase1.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = optimizedDatabase1(inputArg1,inputArg2)
+%OPTIMIZEDDATABASE1 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
